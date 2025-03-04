297f68aa58f33ece04ecff04fdcf40cd424a2939
admin <mametkadyrov.y@gmail.com>
2025-03-04 17:35:26 +0300

    spring-mvc

---
+++ src/main/java/web/controller/Car.java
@@ -0,0 +1,53 @@
+package web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class Car {
+
+    private String brand;
+    private String model;
+    private int year;
+
+    public Car() {
+    }
+
+    public Car(String brand, String model, int year) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @GetMapping(value = "/cars")
+    public String printCars(ModelMap model, @RequestParam int count) {
+        model.addAttribute("cars", MyService.getCars(count));
+        return "cars";
+    }
+}
